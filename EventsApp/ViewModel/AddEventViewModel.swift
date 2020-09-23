5b07b075936247ccb21ad17661a97dfd40347e60
raulgarcia <garcia.raul@gmail.com>
2020-09-23 19:46:24 -0400

    TableView cell for AddEvents created with code

--- EventsApp/ViewModel/AddEventViewModel.swift
+++ EventsApp/ViewModel/AddEventViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Saulo Garcia on 9/23/20.
 //
 
-import Foundation
+import UIKit
 
 final class AddEventViewModel {
     var coordinator: AddEventCoordinator?
@@ -35,4 +35,45 @@ final class TitleSubtitleCellViewModel {
         self.placeHolder = placeHolder
     }
     
+}
+
+final class TitleSubtitleCell: UITableViewCell {
+    private let titleLabel = UILabel()
+    private let subtitleTextfield = UITextField()
+    private let verticalStackView = UIStackView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // This means we are not going to be using the storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        verticalStackView.axis = .vertical
+        titleLabel.font = .systemFont(ofSize: 22, weight: .medium)
+        subtitleTextfield.font = .systemFont(ofSize: 20, weight: .medium)
+    }
+    
+    private func setupHierarchy() {
+        contentView.addSubview(verticalStackView)
+        verticalStackView.addArrangedSubview(titleLabel)
+        verticalStackView.addArrangedSubview(subtitleTextfield)
+    }
+    
+    // Setup constraints
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            verticalStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            verticalStackView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            verticalStackView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            verticalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
 }
